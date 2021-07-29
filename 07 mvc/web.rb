ce8072516b9198d24e76977cc49ad41e62eabe2d
Erma Safira N <erma.safira@gmail.com>
2021-07-29 15:14:32 +0700

    Add feature: create item

--- 07 mvc/web.rb
+++ 07 mvc/web.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require_relative 'models/item'
+require_relative 'models/category'
 
 get '/' do
     items = Item.find_all_items_with_category
@@ -13,4 +14,22 @@ get '/items/:id/show' do
     erb :show, locals: {
         item: item
     }
+end
+
+get '/items/new' do
+    categories = Category.find_all_categories
+    erb :create, locals: {
+        categories: categories
+    }
+end
+
+post '/items/create' do
+    name =  params['name']
+    price =  params['price']
+    category_id = params['category_id'].to_i
+
+    category = Category.find_by_id(category_id)
+    Item.create(name, price, category)
+
+    redirect '/'
 end
