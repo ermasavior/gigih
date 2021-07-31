fc60f172634c65854304a8859d7eee143efd8f00
Erma Safira N <erma.safira@gmail.com>
2021-07-31 19:04:05 +0700

    Add category spec: create

--- 07 mvc/spec/models/category_spec.rb
+++ 07 mvc/spec/models/category_spec.rb
@@ -18,6 +18,30 @@ RSpec.describe Category do
     end
 
     describe '.create' do
+        let(:db_connector) { double }
+
+        context 'when parameters are valid' do
+            let(:name) { "Street food" }
+
+            it 'triggers category creation query' do
+                expect(Category.client).to receive(:query)
+                expect(Category.client).to receive(:last_id)
+
+                category = Category.create(name)
+                expect(category.name).to eq(name)
+            end
+        end
+
+        context 'when parameters are invalid' do
+            let(:name) { nil }
+
+            it 'does not trigger category creation' do
+                expect(Category.client).not_to receive(:query)
+
+                category = Category.create(name)
+                expect(category).to be(nil)
+            end
+        end
     end
 
     describe '.update' do
