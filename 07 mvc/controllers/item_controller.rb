8b9a53d777e874e9d83ca970995785aee3314985
Erma Safira N <erma.safira@gmail.com>
2021-07-31 12:00:55 +0700

    Refactor item views

--- 07 mvc/controllers/item_controller.rb
+++ 07 mvc/controllers/item_controller.rb
@@ -4,26 +4,27 @@ require_relative '../models/category'
 class ItemController
     def list_items
         items = Item.find_all_items_with_category
+        categories = Category.find_all_categories
         renderer = ERB.new(File.read("./views/index.erb"))
         renderer.result(binding)
     end
 
     def item_detail(params)
         item = Item.find_by_id(params['id'])
-        renderer = ERB.new(File.read("./views/show.erb"))
+        renderer = ERB.new(File.read("./views/items/show.erb"))
         renderer.result(binding)
     end
 
     def create_item_form
         categories = Category.find_all_categories
-        renderer = ERB.new(File.read("./views/create.erb"))
+        renderer = ERB.new(File.read("./views/items/create.erb"))
         renderer.result(binding)
     end
 
     def update_item_form(params)
         item = Item.find_by_id(params['id'])
         categories = Category.find_all_categories
-        renderer = ERB.new(File.read("./views/update.erb"))
+        renderer = ERB.new(File.read("./views/items/update.erb"))
         renderer.result(binding)
     end
 
